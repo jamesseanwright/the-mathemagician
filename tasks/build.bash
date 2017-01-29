7fd7b07319fda5fb38d34b7c2d3281e7ca4073e5
James Wright <james@jamesswright.co.uk>
2017-01-29 11:38:04 +0000

    Output byte lengths of minified and crushed output

--- tasks/build.bash
+++ tasks/build.bash
@@ -1,5 +1,13 @@
 #!/usr/bin/env bash
 
+function echo_byte_count {
+    local type=$1
+    local js=$2
+    local byte_length=$(echo $js | wc --bytes)
+
+    echo "$type output length: $byte_length byte(s)"
+}
+
 [ -e dist.html ] && rm dist.html
 
 echo "Minifying with Closure Compiler..."
@@ -8,12 +16,15 @@ minified=$(./node_modules/.bin/google-closure-compiler-js \
     --compilationLevel ADVANCED \
     src/index.js)
 
+echo_byte_count "Minified" $minified
+
 # interestingly, invoking ./node_modules/.bin/regpack
 # results in ': No such file or directory'. I can't
 # find the cause of this, so I'm explicitly running
 # it through Node.js
 echo "Crushing minified output with RegPack..."
 crushed=$(echo $minified | node ./node_modules/.bin/regpack -)
+echo_byte_count "Crushed" $crushed
 
 echo "Injecting crushed script into index.html"
 sed "s/###DEMOTARGET###/$crushed/" src/index.html > dist.html
