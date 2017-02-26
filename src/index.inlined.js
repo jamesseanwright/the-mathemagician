a77b349da6d73765160ac3aefbe25a62e23e4129
James Wright <james@jamesswright.co.uk>
2017-02-26 12:38:51 +0000

    Further inlining 97 bytes over

--- src/index.inlined.js
+++ src/index.inlined.js
@@ -1,39 +1,34 @@
 var audioContext = new AudioContext();
 
-var mathMagicianX = a.width / 2;
+var mathMagicianX = 512;
 var mathMagicianY = 200;
-var mathMagicianHeadRadius = 45;
 var bounceDirection = 1;
 
-var leftOperand = Math.round(Math.random() * 50000);
-var rightOperand = Math.round(Math.random() * 50000);
-var result = leftOperand * rightOperand;
 var lastMultiplicationGenTime = null;
 
 var bgX = 0;
 
 // create background elements
-for (var x = 0; x + 35 < a.width; x += 35) {
-    for (var y = 0; y < a.height; y += 35) {
+for (var x = 0; x + 35 < 1024; x += 35) {
+    for (var y = 0; y < 720; y += 35) {
         c.fillStyle = 'rgba(' + [
-            Math.ceil(Math.random() * 255),
-            Math.ceil(Math.random() * 255),
-            Math.ceil(Math.random() * 255),
+            Math.random() * 255 | 0,
+            Math.random() * 255 | 0,
+            Math.random() * 255 | 0,
         ].join(',') + ',0.5)'
 
         c.font = '25px monospace';
-        c.fillText(Math.round(Math.random() * 9), x, y, 25);
+        c.fillText(Math.random() * 9 | 0, x, y, 25);
     }
 }
 
-bgImageData = c.getImageData(0, 0, a.width, a.height);
+bgImageData = c.getImageData(0, 0, 1024, 720);
 a.style.background = '#000';
 
 var snareBuffer = audioContext.createBuffer(1, audioContext.sampleRate, audioContext.sampleRate);
-var snareBufferOutput = snareBuffer.getChannelData(0);
 
 for (var i = 0; i < audioContext.sampleRate; i++) {
-    snareBufferOutput[i] = Math.random() * 2 - 1;
+    snareBuffer.getChannelData(0)[i] = Math.random() * 2 - 1;
 }
 
 // enqueue, play, and loop freqs
@@ -72,20 +67,20 @@ for (var i = 0; i < audioContext.sampleRate; i++) {
 }());
 
 requestAnimationFrame(function loop(time) {
-    c.clearRect(0, 0, a.width, a.height);
+    c.clearRect(0, 0, 1024, 720);
 
     // bg animation
     bgX -= 0.5;
     c.putImageData(bgImageData, bgX, 0);
-    c.putImageData(bgImageData, bgX + a.width, 0);
+    c.putImageData(bgImageData, bgX + 1024, 0);
 
-    if (bgX + a.width < 0) {
+    if (bgX < -1024) {
         bgX = 0;
     }
 
     // Mathmagician render
     // bounce animation
-    if (mathMagicianY + mathMagicianHeadRadius > 350 && bounceDirection === 1) {
+    if (mathMagicianY + 45 > 350 && bounceDirection === 1) {
         bounceDirection = -1;
     }
 
@@ -94,34 +89,34 @@ requestAnimationFrame(function loop(time) {
     }
 
     var progress = 150 / (mathMagicianY || 1);
-    mathMagicianY += (3 * bounceDirection) * Math.sin(Math.PI * progress);
+    mathMagicianY += (3 * bounceDirection) * Math.sin(3.14 * progress);
 
     // head
     c.fillStyle = '#fff6e5';
 
     c.beginPath();
-    c.ellipse(mathMagicianX, mathMagicianY, mathMagicianHeadRadius, mathMagicianHeadRadius, 0, 0, Math.PI * 2);
+    c.ellipse(mathMagicianX, mathMagicianY, 45, 45, 0, 0, 6.2832);
     c.fill();
 
     // left eye
     c.fillStyle = '#000';
     c.beginPath();
-    c.ellipse(mathMagicianX - 25, mathMagicianY, 5, 15, 0, 0, Math.PI * 2);
+    c.ellipse(mathMagicianX - 25, mathMagicianY, 5, 15, 0, 0, 6.2832);
     c.fill();
 
     // right eye
     c.beginPath();
-    c.ellipse(mathMagicianX + 25, mathMagicianY, 5, 15, 0, 0, Math.PI * 2);
+    c.ellipse(mathMagicianX + 25, mathMagicianY, 5, 15, 0, 0, 6.2832);
     c.fill();
 
     // hat
     c.fillStyle = '#104E8B';
 
     c.beginPath();
-    c.moveTo(mathMagicianX - mathMagicianHeadRadius, mathMagicianY - mathMagicianHeadRadius / 2);
-    c.lineTo(mathMagicianX, mathMagicianY - mathMagicianHeadRadius - 70);
-    c.lineTo(mathMagicianX + mathMagicianHeadRadius, mathMagicianY - mathMagicianHeadRadius / 2);
-    c.lineTo(mathMagicianX - mathMagicianHeadRadius, mathMagicianY - mathMagicianHeadRadius / 2);
+    c.moveTo(mathMagicianX - 45, mathMagicianY - 45 / 2);
+    c.lineTo(mathMagicianX, mathMagicianY - 45 - 70);
+    c.lineTo(mathMagicianX + 45, mathMagicianY - 45 / 2);
+    c.lineTo(mathMagicianX - 45, mathMagicianY - 45 / 2);
     c.fill();
 
     // End Mathmagician
@@ -129,13 +124,13 @@ requestAnimationFrame(function loop(time) {
     // multiplication render
     if (!lastMultiplicationGenTime || time - lastMultiplicationGenTime > 4000) {
         lastMultiplicationGenTime = time;
-        leftOperand = Math.round(Math.random() * 50000);
-        rightOperand = Math.round(Math.random() * 50000);
+        leftOperand = Math.random() * 50000 | 0;
+        rightOperand = Math.random() * 50000 | 0;
         result = leftOperand * rightOperand;
     }
 
     progress = (time - lastMultiplicationGenTime) / 4000;
-    var rotation = Math.PI * 0.5 - progress * 2;
+    var rotation = 1.57 - progress * 2;
     var scale = 1 + progress * 2;
     var opacity = 1 - progress;
 
@@ -143,7 +138,7 @@ requestAnimationFrame(function loop(time) {
     c.scale(scale, scale);
 
     c.fillStyle = 'rgba(255, 255, 255, ' + opacity + ')';
-    c.font = 'bold 26px Arial';
+    c.font = 'bold 25px Arial';
     c.fillText(leftOperand + ' x ' + rightOperand + ' = ' + result, 150, 50);
     c['resetTransform'](); // It seems Closure Compiler isn't aware of this relatively new API
 
